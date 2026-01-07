4af8c1a597947a3d67b51ce379a32ac74c4775d9
Besthope <42211042@smail.swufe.edu.cn>
2026-01-07 12:33:16 +0800

    feat: update automatically on row modifications (#86)
    * feat: update automatically on row modifications
    
    * refactor: convert tests to use async session and fixtures

--- src/shared/mixins.py
+++ src/shared/mixins.py
@@ -5,4 +5,7 @@ from sqlmodel import Field
 
 class TimestampMixin:
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)},
+    )
